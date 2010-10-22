75acbffdb9637322f07fa606c74aa291480bf0e5
ilya <ilya@laptop.(none)>
2010-10-21 19:35:35 -0700

    started the new invitation email

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,22 +1,20 @@
 class Notifier < ActionMailer::Base
   default :from => "no-reply@joindiaspora.com"
-  ATTACHMENT =  File.read("#{Rails.root}/public/images/diaspora_caps.png")  
-
+  
   def new_request(recipient, sender)
     @receiver = recipient
     @sender = sender
-    attachments["diaspora_white.png"] = ATTACHMENT 
-
+<<<<<<< HEAD
     mail(:to => "#{recipient.real_name} <#{recipient.email}>",
-    :subject => "new Diaspora* friend request from #{@sender.real_name}", :host => APP_CONFIG[:terse_pod_url])
+    :subject => "new Diaspora* friend request from #{@sender.real_name}")
+  end
+=======
+    mail(:to => recipient.email) do |format|
+      format.text { render :text => "This is text!" }
+      format.html { render :text => "<h1>#{@receiver.person.profile.first_name}This is HTML</h1>" }
+    end
   end
 
-  def request_accepted(recipient, sender, aspect)
-    @receiver = recipient
-    @sender = sender
-    @aspect = aspect
-    attachments["diaspora_white.png"] = ATTACHMENT 
-    mail(:to => "#{recipient.real_name} <#{recipient.email}>",
-    :subject => "#{@sender.real_name} has accepted your friend request on Diaspora*", :host => APP_CONFIG[:terse_pod_url])
-  end
+
+>>>>>>> started the new invitation email
 end
--- spec/mailers/notifier_spec.rb
+++ spec/mailers/notifier_spec.rb
@@ -3,12 +3,15 @@ require 'spec_helper'
 
 describe Notifier do
 
+<<<<<<< HEAD
   let!(:user) {Factory.create :user}
-  let!(:aspect) {user.aspect(:name => "science")}
   let!(:person) {Factory.create :person}
   let!(:request_mail) {Notifier.new_request(user, person)}
-  let!(:request_accepted_mail) {Notifier.request_accepted(user, person, aspect)}
-
+=======
+  let(:user) {Factory.create :user}
+  let(:person) {Factory.create :person}
+  let(:request_mail) {Notifier.new_request(user, person)}
+>>>>>>> started the new invitation email
 
   describe "#new_request" do
     it 'goes to the right person' do
@@ -16,31 +19,16 @@ describe Notifier do
     end
 
     it 'has the receivers name in the body' do
+<<<<<<< HEAD
       request_mail.body.encoded.include?(user.person.profile.first_name).should be true
     end
 
 
     it 'has the name of person sending the request' do
       request_mail.body.encoded.include?(person.real_name).should be true
-    end
-  end
-
-  describe "#request_accpeted" do
-    it 'goes to the right person' do
-      request_accepted_mail.to.should == [user.email]
-    end
-
-    it 'has the receivers name in the body' do
-      request_accepted_mail.body.encoded.include?(user.person.profile.first_name).should be true
-    end
-
-
-    it 'has the name of person sending the request' do
-      request_accepted_mail.body.encoded.include?(person.real_name).should be true
-    end
-
-    it 'has the name of the aspect in the body' do
-      request_accepted_mail.body.encoded.include?(aspect.name).should be true
+=======
+      request_mail.body.encoded.includes?(user.first_name).should be true
+>>>>>>> started the new invitation email
     end
   end
 end
