8dcbebdae18930a018f3afa39c78c75f14e2d4b1
Tyler Reece <mtreece@ncsu.edu>
2010-10-31 22:04:55 -0400

    Semi-working progress for signed-time authentication!

--- app/controllers/updates_controller.rb
+++ app/controllers/updates_controller.rb
@@ -11,7 +11,64 @@ class UpdatesController < ApplicationController
   require 'time'
   before_filter :authenticate_user!, :except => [:new, :create, :public, :import]
 
-  # for now, let us forget the timestamp checking
+  
+  def get_updates 
+    #let's get the person_id and forget about authentication for a while
+    # @updates = getHashOfUpdatesSince(:timestamp);
+    
+    # authentication!
+    # URL will be ...
+    # /updates?personid=PERSONID&timestamp=TIMESTAMP&token=TOKEN
+    # ... where TOKEN = PRIVATE_KEY.encrypt(UNIX EPOCH TIME)
+    # 
+    
+    # If the authentication fails, render "Failed." for testing
+    # purposes.
+    
+    person = Person.find_by_id(params[:personid])
+    
+    sig = unix_signature
+    
+    s = "<a href=\"http://localhost:3000/updates?timestamp=asdf&personid=4cc3d4c627391054b3000005&token=#{sig}\">http://localhost:3000/updates?timestamp=asdf&personid=4cc3d4c627391054b3000005&token=#{sig}</a><br /><br /><br />"
+    
+    render :inline => s + user_authentic_token?(person, params[:token])  
+    
+    return
+    
+    
+    if !person || !user_authentic_token?(person, params[:token])
+      render :inline => "Failed."
+      
+      
+      person.public_key.public_decrypt(Base64.decode64 params[:token].gsub(" ", "+"))
+      
+      
+      
+      return
+    end
+    
+    allposts = getListOfUpdatesSince(params[:timestamp])
+    
+    for newpost in allposts do 
+      #send each post
+      #see if the username does exist or not
+        
+      if current_user.isPostForPerson?(newpost, params[:personid])
+        render :json => current_post
+        current_user.push_to_people(newpost, User.find_by_username(params[:personid]).person )  
+      end
+    end
+    
+    #render :json => allposts
+    #render :json => current_user.person.id
+    #render :nothing => true # will render nothing when we want it to...
+    render :inline => "Success."
+  end 
+
+
+  
+  private
+  
   def getListOfUpdatesSince(timestamp)
     
     latestUpdates = []
@@ -23,33 +80,45 @@ class UpdatesController < ApplicationController
     end
     latestUpdates
   end
-
-  def get_updates 
-    #let's get the person_id and forget about authentication for a while
-    # @updates = getHashOfUpdatesSince(:timestamp);
-    allposts = getListOfUpdatesSince(params[:timestamp])
-    
-    for newpost in allposts do 
-      #send each post
-      #see if the username does exist or not
-        
-     if Person.find_by_id(params[:personid])
-      if current_user.isPostForPerson?(newpost, params[:personid])
-        render :json => current_post
-        current_user.push_to_people(newpost, User.find_by_username(params[:personid]).person )  
-      end
-    end
-   end
-    
-    #render :json => allposts
-    render :nothing => true # will render nothing when we want it to...
-    #render :json => current_user.person.id
   
-  end 
-
-
+  def user_authentic_token?(person, token)
+    ### to play with the encryption / decryption...
+    #
+    #        time = Time.now.to_i.to_s
+    #        plain = "plain: " + time
+    #        cipher = "cipher: " + person.encrypt(plain)
+    #        #decr = "decr: " + current_user.aes_decrypt(person.encrypt(plain), current_user.gen_aes_key)
+    #        decr = "decr: " + current_user.decrypt (person.encrypt(plain))
+    #        final = plain + "<br />" + cipher + "<br />" + decr + "<br />"
+    #        render :inline => final
+    
+    # token get string replaces "+" with " ", so replace them back
+    signed = token.gsub(" ", "+")
+    
+    result = person.public_key.public_decrypt(Base64.decode64 signed)
+    
+    result
+  end
+  
+  # grab the signed value of the unix-time, for generating update-tokens
+  def unix_signature
+    # get the current user's private key
+    key = current_user.encryption_key
+    
+    # get the current unix epoch time 
+    unix_time = Time.now.to_i.to_s
+    
+    # sign the current unix epoch time with my private key
+    unix_encrypted = key.private_encrypt(unix_time)
+    
+    # convert to base64 for URL-friendliness 
+    unix_encrypted_base64 = Base64.encode64 unix_encrypted 
+    
+    # strip out the newlines for the final unix signature, for good measure and safety
+    unix_sig = unix_encrypted_base64.gsub("\n", "")
+  end
+  
   # everything after this is unneeded -- just copied/pasted for the example
-  private
   def edit
     @aspect  = :user_edit
     @user    = current_user
